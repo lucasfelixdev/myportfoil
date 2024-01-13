2ac98fd8863caafe6d2df8577b4c552d77604f29
Lukas Félix <lucas2009jesus@gmail.com>
2024-01-13 20:03:57 -0300

    Update - Js archive

---
+++ script/reveal.js
@@ -0,0 +1,35 @@
+window.revelar = ScrollReveal({
+    reset:true
+})
+
+// CARDS PROJETOS
+revelar.reveal('.card-top', 
+{
+    duration: 2500,
+    distance: '80px',
+    origin: 'top'
+})
+
+revelar.reveal('.card-top1', 
+{
+    duration: 2500,
+    distance: '80px',
+    delay: 100,
+    origin: 'top'
+})
+
+revelar.reveal('.card-top2', 
+{
+    duration: 2500,
+    distance: '80px',
+    delay: 200,
+    origin: 'top'
+})
+
+revelar.reveal('.card-top3', 
+{
+    duration: 2500,
+    distance: '80px',
+    delay: 300,
+    origin: 'top'
+})
